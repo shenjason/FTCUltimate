695ecd3f3e601f49297e6d66b43e5ea4e446360b
shenjason <83889944+shenjason@users.noreply.github.com>
2026-04-02 20:26:16 -0700

    Revert "miscfix"
    This reverts commit 63499c16a83c285cd33b36a403503199d0dcd007.

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // Use expo-router's entry to bootstrap the app and provide navigation.
 // This replaces a manual App registration so the file-based router works.
-import "expo-router/entry";
+import 'expo-router/entry';
